fc378d745b83ddde05d834d2b52a978866b34a46
NJR <njr@MacBookPro.Dlink>
2026-02-12 22:57:09 +0200

    Completed loop lists challenge

---
+++ loop_lists.py
@@ -0,0 +1,6 @@
+# loop_lists.py
+
+movies = ["Inception", "Interstellar", "The Dark Knight", "Gladiator", "Titanic"]
+
+for index, movie in enumerate(movies, start=1):
+    print(f"Movie {index}: {movie}")
