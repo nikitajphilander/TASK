7a9d1ea50edd6f4e66062f5dcbb2a9d8e42ba682
NJR <njr@MacBookPro.Dlink>
2026-02-12 20:43:49 +0200

    Fixed syntax and runtime errors in errors.py

---
+++ errors.py
@@ -0,0 +1,9 @@
+# errors.py
+
+print("Welcome to the calculator")  # Syntax Error - missing closing bracket
+
+number = int(input("Enter a number: "))  # Runtime Error - input must be converted to integer
+
+result = number / 2
+
+print("Half of your number is " + str(result))
