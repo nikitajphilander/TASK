5360d6bd03c2bc41fd1a2144cef78034b1f8e15c
NJR <njr@MacBookPro.Dlink>
2026-02-12 20:45:02 +0200

    Added program with logical error

---
+++ logic.py
@@ -0,0 +1,12 @@
+# logic.py
+
+age = 18
+
+if age > 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+# Logical Error:
+# If age is 18, it prints "minor", which is incorrect.
+# The condition should be >= 18.
