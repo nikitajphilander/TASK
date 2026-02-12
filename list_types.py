dc35fa5398c82d4a8c21dfe38d2071efc172dc99
NJR <njr@MacBookPro.Dlink>
2026-02-12 17:20:12 +0200

    Add list types task

---
+++ list_types.py
@@ -0,0 +1,20 @@
+# Create a list of three friends' names
+friends_names = ["Sophia", "Liam", "Ava"]
+
+# Print the first friend
+print("First friend:", friends_names[0])
+
+# Print the last friend
+print("Last friend:", friends_names[-1])
+
+# Print the length of the list
+print("Number of friends:", len(friends_names))
+
+
+# Create a corresponding list of ages
+friends_ages = [25, 27, 24]
+
+# Print each friend's name and age
+print(f"{friends_names[0]} is {friends_ages[0]} years old")
+print(f"{friends_names[1]} is {friends_ages[1]} years old")
+print(f"{friends_names[2]} is {friends_ages[2]} years old")
