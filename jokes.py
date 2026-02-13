b4e4d32fe13349ae89b67330d4ff8a9b09f4471d
Your Name <your@email.com>
2026-02-13 14:26:26 +0200

    Completed random joke generator task

---
+++ jokes.py
@@ -0,0 +1,15 @@
+# jokes.py
+
+import random
+
+jokes = [
+    "Why don’t programmers like nature? It has too many bugs.",
+    "Why did the computer go to therapy? It had too many bytes of trauma.",
+    "Why do Python programmers wear glasses? Because they can't C.",
+    "Why was the JavaScript developer sad? Because he didn’t Node how to Express himself.",
+    "Why did the developer go broke? Because he used up all his cache."
+]
+
+random_joke = random.choice(jokes)
+
+print(random_joke)
